063dfe92ffc23c84b8ee21d5f8bf276abbb24a6b
apoptis <travis.nffnsnc@gmail.com>
2014-11-17 02:59:53 -0500

    sunburst added, slider changed

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('div.container-full').pow({
+  rays: 16, // positive integer > 1
+  bgColorStart: '#3499DB', // any css color
+  rayColorStart: '#0F85D1', // any css color
+  bgColorEnd: '#58ACE3', // any css color
+  rayColorEnd: '#3499DB', // any css color
+  originX: '35%', // percentage, e.g. '33%'
+  originY: '20%' // percentage, e.g. '120%'
+ 
+  // note: 'originEl' overrides 'originX' and 'originY'
+});
