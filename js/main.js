5efea2b4571402411a10a94726644bd700745516
apoptis <travis.nffnsnc@gmail.com>
2014-11-19 02:15:27 -0500

    carousel changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//sunburst
+
 $('div.container-full').pow({
   rays: 16, // positive integer > 1
   bgColorStart: '#3499DB', // any css color
@@ -8,4 +10,24 @@ $('div.container-full').pow({
   originY: '20%' // percentage, e.g. '120%'
  
   // note: 'originEl' overrides 'originX' and 'originY'
+});
+
+//carousel
+
+$(document).ready(function() {    
+    //Events that reset and restart the timer animation when the slides change
+    $("#transition-timer-carousel").on("slide.bs.carousel", function(event) {
+        //The animate class gets removed so that it jumps straight back to 0%
+        $(".transition-timer-carousel-progress-bar", this)
+            .removeClass("animate").css("width", "0%");
+    }).on("slid.bs.carousel", function(event) {
+        //The slide transition finished, so re-add the animate class so that
+        //the timer bar takes time to fill up
+        $(".transition-timer-carousel-progress-bar", this)
+            .addClass("animate").css("width", "100%");
+    });
+    
+    //Kick off the initial slide animation when the document is ready
+    $(".transition-timer-carousel-progress-bar", "#transition-timer-carousel")
+        .css("width", "100%");
 });
