e66661e2808805d11fdc616082b5137466eeca84
apoptis <travis.nffnsnc@gmail.com>
2014-11-22 16:34:43 -0500

    about section improvements

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $('div.container-full').pow({
   rayColorStart: '#0F85D1', // any css color
   bgColorEnd: '#58ACE3', // any css color
   rayColorEnd: '#3499DB', // any css color
-  originX: '35%', // percentage, e.g. '33%'
+  originX: '50%', // percentage, e.g. '33%'
   originY: '20%' // percentage, e.g. '120%'
  
   // note: 'originEl' overrides 'originX' and 'originY'
