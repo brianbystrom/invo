7dee9aa87a944cbbdb9b495e68aa5d7d5065142d
apoptis <travis.nffnsnc@gmail.com>
2015-01-27 20:01:04 -0500

    fitText implemented (auctime aucprice)

--- js/main.js
+++ js/main.js
@@ -31,6 +31,45 @@ $('div.orangeburst').pow({
 });
 
 
+//fitText
+
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
+
+jQuery(".auctime").fitText(.4, { maxFontSize: '40px'});
+jQuery(".aucprice").fitText(.3, { minFontSize: '20px', maxFontSize: '40px'});
+
+
 //carousel
 
 $(document).ready(function() {    
